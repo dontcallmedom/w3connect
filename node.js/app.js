0480074e83e1c2befdbbabf1bde263361f8f3778
Dominqique Hazael-Massieux <dom@w3.org>
2011-08-24 23:11:28 +0200

    tpacweb/app.js

--- node.js/app.js
+++ node.js/app.js
@@ -294,5 +294,5 @@ app.get('/people.:format?', function (req, res){
 });
 
 
-app.listen(80);
+app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
