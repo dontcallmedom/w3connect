5c04dba7b11f37e8d6affab40b41016132470655
Dominique Hazaël-Massieux <dom@w3.org>
2011-08-26 10:38:09 +0200

    less verbose

--- maps/build.sh
+++ maps/build.sh
@@ -4,7 +4,7 @@ ROOMS_IDS=`cat rooms.json|cut -d ":" -f 2|cut -d "," -f 1|cut -d '"' -f 2|grep -
 ROOMS_SVG=`for i in $ROOMS_IDS; do echo "$i.svg" ; done`
 
 for i in $ROOMS_SVG
-do echo $i ;cat Santa_Clara_Marriott_Map.svg |sed -e 's/"text\/css">/"text\/css">#'`basename $i .svg`' { fill: yellow;}/' > $i; inkscape $i -e ../node.js/public/maps/`basename $i .svg`.png 2>/dev/null ; rm $i
+do cat Santa_Clara_Marriott_Map.svg |sed -e 's/"text\/css">/"text\/css">#'`basename $i .svg`' { fill: yellow;}/' > $i; inkscape $i -e ../node.js/public/maps/`basename $i .svg`.png 2>/dev/null ; rm $i
 done
 
 cp Santa_Clara_Marriott_Map.svg ../node.js/public/maps/main.svg
