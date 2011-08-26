1c84dedd8dd8bd5f2c05d6175c58a1efd7815513
Dominique Hazaël-Massieux <dom@w3.org>
2011-08-26 10:37:55 +0200

    minor bug fix

--- maps/build.sh
+++ maps/build.sh
@@ -1,9 +1,10 @@
 # depends on inkscape to generate PNG versions
 
-ROOMS_SVG=`for i in \`cat rooms.json|cut -d ":" -f 2|cut -d "," -f 1|cut -d '"' -f 2|grep -v "{"|grep -v "}"\`; do echo "$i.svg" ; done`
+ROOMS_IDS=`cat rooms.json|cut -d ":" -f 2|cut -d "," -f 1|cut -d '"' -f 2|grep -v "{"|grep -v "}"`
+ROOMS_SVG=`for i in $ROOMS_IDS; do echo "$i.svg" ; done`
 
-for i in $ROOMS_SVG:
-do cat Santa_Clara_Marriott_Map.svg |sed -e 's/"text\/css">/"text\/css">#'`basename $i .svg`' { fill: yellow;}/' > $i; inkscape $i -e ../node.js/public/maps/`basename $i .svg`.png ; rm $i
+for i in $ROOMS_SVG
+do echo $i ;cat Santa_Clara_Marriott_Map.svg |sed -e 's/"text\/css">/"text\/css">#'`basename $i .svg`' { fill: yellow;}/' > $i; inkscape $i -e ../node.js/public/maps/`basename $i .svg`.png 2>/dev/null ; rm $i
 done
 
-cp Santa_Clara_Marriott_Map.svg ../node.Js/public/maps/main.svg
+cp Santa_Clara_Marriott_Map.svg ../node.js/public/maps/main.svg
