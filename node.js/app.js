d1afebd8ef73476fa321a0e5937a028fcf7090bd
Dominqique Hazael-Massieux <dom@w3.org>
2011-08-29 00:20:10 +0200

    moved import people into separate module

--- node.js/app.js
+++ node.js/app.js
@@ -6,6 +6,8 @@
 var express = require('express');
 var everyauth = require('everyauth'),
     EventEmitter = require('events').EventEmitter;
+var imports = require("./imports.js");
+
 
 form = require("express-form"),
 filter = form.filter,
@@ -22,34 +24,9 @@ var People = require('./model.js').People(db);
 
 var Organization = require('./model.js').Organization(db);
 var Place = require('./model.js').Place(db);
-var Settings = require('./model.js').Settings(db);
 var TaxiFromAirport = require('./model.js').TaxiFromAirport(db);
 var TaxiToAirport = require('./model.js').TaxiToAirport(db);
 
-function loadPeopleData(id) {
-   var http = require('http');
-   var request = http.get({host: 'www.w3.org', path:'/2011/08/w3c-data/people/' + id}, function (response) {
-     response.setEncoding('utf8');
-     var peopleJSON = "", peopleData;
-     response.on('data', function (chunk) {
-       peopleJSON = peopleJSON + chunk;
-     });
-     response.on('end', function () {
-       peopleData = JSON.parse(peopleJSON);
-       People.findOne({w3cId: id}, function(err, people) {
-           if (people && people.picture != peopleData.picture) {
-	       people.picture = peopleData.picture;
-	       people.save();
-	   }
-           if (people && people.picture_thumb != peopleData.thumbnail) {
-	       people.picture_thumb = peopleData.thumbnail;
-	       people.save();
-	   }
-
-       });
-     });
-  });
-}
 
 
 
@@ -168,98 +145,41 @@ app.error(function(err, req, res, next){
 
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'TPAC Web App'
+  People.count({}, function(err, count) {
+      if (!count) {
+	  // no user, need to import data
+	  if (! req.loggedIn) {
+	      return res.redirect(everyauth.password.getLoginPath());
+	  } else {
+	      // Import basic data: people and rooms
+
+	  }
+      } else {
+	  res.render('index');
+      }
   });
 });
 
 app.get('/admin/', function(req, res){
-  res.render('admin/index', {
-    title: 'TPAC Web App Administration'
-  });
+    res.render('admin/index');
+
 });
 
 app.post('/admin/', function(req, res){
   if (req.body.peopleUpdate) {
     if (! req.loggedIn) {
-      req.session.redirectTo = '/locations/' + req.params.id;
       return res.redirect(everyauth.password.getLoginPath());
-     }
-   var https = require('https');
-
-   var request = https.get({host: 'www.w3.org', path:'/2002/09/wbs/tpRegistrants-json.php?wgid=35125&qaireno=TPAC2011', headers: {Authorization: 'Basic ' + app.set('w3c_auth')}}, function (response) {
-     response.setEncoding('utf8');
-     var registrantsJSON = "", registrantsData;
-     response.on('data', function (chunk) {
-       registrantsJSON = registrantsJSON + chunk;
-     });
-     response.on('end', function () {
-        registrantsData = JSON.parse(registrantsJSON);
-        var additions = {orgs:[],people:[]}, errors;
-        var counter = 0, counterAdded = 0;
-        function addOrg(org, people) {
-	    return function (err) {
-		// assume duplicate key errors
-	        if (err) {
-		    Organization.findOne({w3cId: people.affiliationId}, ["_id", "name"], function(err, org) {
-		    if (org) {
-			people.affiliation = org._id;
-		    }
-     	            people.save(addPeople(people));		    
-		    });
-		} else {
-                    req.flash('info', org.name + ' added');
-		    people.affiliation = org._id;
-     	            people.save(addPeople(people));		      
-		}
-	    };
-	}
-        function addPeople(people) {
-  	  return function (err) {
-               counter++;
-		// We ignore duplicate key errors
-	       if (!err) {
-                 counterAdded++;
-                 req.flash('info', people.given + ' ' + people.family + ' added');
-		 // update list of employees
-  	         Organization.findById(people.affiliation
-                     // closure to add employee to org record
-				    , (function (p) {
-					return function (err, org) {
-					if (org) {
-					    org.employees.push(people._id);
-					    org.save();
-					}};})(people) );
-	       }
-       	       if (counter == registrantsData.registrants.length) {
-		if (!counterAdded) {
-		    req.flash('info', 'No new data to import');
-		}
-                res.render('admin/index');
-	       }
-	     };
-        }
-        for (p in registrantsData.registrants) {
-            var peopleData = registrantsData.registrants[p];
-	    var people = new People();
-	    people.given = peopleData.given;
-	    people.family = peopleData.family;
-	    people.email = peopleData.email;
-	    people.login = peopleData.login;
-	    people.w3cId = peopleData.w3cId;
-	    loadPeopleData(people.w3cId);
-	    if (peopleData.organization && peopleData.organization.w3cId) {
-		     people.affiliationId = peopleData.organization.w3cId;
-  		     org = new Organization();
-		     org.w3cId = peopleData.organization.w3cId;
-		     org.name = peopleData.organization.name;
-		     org.save(addOrg(org, people));		       
-	    } else {
-  	      people.save(addPeople(people));		
-	    }
-	}
-     });
-   });
+    } else if (!app.set("w3c_auth")) {
+	// in case user logged in a previous session
+	// should find how to logout?
+	return res.redirect(everyauth.password.getLoginPath());
+    }
+      imports.importUserList(app.set("w3c_auth"), function(success, info, errors) {
+	  if (success) success.forEach(function(i) { req.flash('success',i);});
+	  if (info) info.forEach(function(i) { req.flash('info',i);});
+	  if (errors) errors.forEach(function(i) { req.flash('error',i);});
+          res.render('admin/index');
+      });
   } else  if (req.body.placeAdd) {
       var place = new Place();
       place.shortname = req.body.shortname;
---
+++ node.js/imports.js
@@ -0,0 +1,118 @@
+// should get it from elsewhere
+var mongoose = require('mongoose'),
+db = mongoose.connect('mongodb://localhost/tpac');
+
+var People = require('./model.js').People(db);
+var Organization = require('./model.js').Organization(db);
+
+function loadPeopleData(id) {
+   var http = require('http');
+   var request = http.get({host: 'www.w3.org', path:'/2011/08/w3c-data/people/' + id}, function (response) {
+     response.setEncoding('utf8');
+     var peopleJSON = "", peopleData;
+     response.on('data', function (chunk) {
+       peopleJSON = peopleJSON + chunk;
+     });
+     response.on('end', function () {
+       peopleData = JSON.parse(peopleJSON);
+       People.findOne({w3cId: id}, function(err, people) {
+           if (people && people.picture != peopleData.picture) {
+	       people.picture = peopleData.picture;
+	       people.save();
+	   }
+           if (people && people.picture_thumb != peopleData.thumbnail) {
+	       people.picture_thumb = peopleData.thumbnail;
+	       people.save();
+	   }
+
+       });
+     });
+  });
+}
+
+
+exports.importUserList = function(auth, callback)  {
+   var https = require('https');
+    var success = [];
+    var info = [];
+    var errors = [];
+   var request = https.get({host: 'www.w3.org', path:'/2002/09/wbs/tpRegistrants-json.php?wgid=35125&qaireno=TPAC2011', headers: {Authorization: 'Basic ' + auth}}, function (response) {
+     response.setEncoding('utf8');
+     var registrantsJSON = "", registrantsData;
+     response.on('data', function (chunk) {
+       registrantsJSON = registrantsJSON + chunk;
+     });
+     response.on('end', function () {
+        registrantsData = JSON.parse(registrantsJSON);
+        var additions = {orgs:[],people:[]}, errors;
+        var counter = 0, counterAdded = 0;
+	
+        function addOrg(org, people) {
+	    return function (err) {
+		// assume duplicate key errors
+	        if (err) {
+		    Organization.findOne({w3cId: people.affiliationId}, ["_id", "name"], function(err, org) {
+		    if (org) {
+			people.affiliation = org._id;
+		    }
+     	            people.save(addPeople(people));		    
+		    });
+		} else {
+		    success.push(org.name + ' added');
+		    people.affiliation = org._id;
+     	            people.save(addPeople(people));		      
+		}
+	    };
+	}
+        function addPeople(people) {
+  	  return function (err) {
+               counter++;
+		// We ignore duplicate key errors
+	       if (!err) {
+                 counterAdded++;
+                 success.push(people.given + ' ' + people.family + ' added');
+		 // update list of employees
+  	         Organization.findById(people.affiliation
+                     // closure to add employee to org record
+				    , (function (p) {
+					return function (err, org) {
+					if (org) {
+					    org.employees.push(people._id);
+					    org.save();
+					}};})(people) );
+	       }
+       	       if (counter == registrantsData.registrants.length) {
+		if (!counterAdded) {
+		    info.push('No new data to import');
+		}
+		callback(success, info, errors);
+	       }
+	     };
+        }
+        for (p in registrantsData.registrants) {
+            var peopleData = registrantsData.registrants[p];
+	    var people = new People();
+	    people.given = peopleData.given;
+	    people.family = peopleData.family;
+	    people.email = peopleData.email;
+	    people.login = peopleData.login;
+	    people.w3cId = peopleData.w3cId;
+	    loadPeopleData(people.w3cId);
+	    if (peopleData.organization && peopleData.organization.w3cId) {
+		     people.affiliationId = peopleData.organization.w3cId;
+  		     org = new Organization();
+		     org.w3cId = peopleData.organization.w3cId;
+		     org.name = peopleData.organization.name;
+		     org.save(addOrg(org, people));		       
+	    } else {
+  	      people.save(addPeople(people));		
+	    }
+	}
+     });
+   });
+
+};
+
+exports.importRooms = function() {
+
+};
