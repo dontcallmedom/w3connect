f240a6824404dd6e2c88d25991e4cfb8cfc90746
Dominqique Hazael-Massieux <dom@w3.org>
2011-09-05 00:20:49 +0200

    updated simplistics tests

--- node.js/test/app.test.js
+++ node.js/test/app.test.js
@@ -1,10 +1,14 @@
+// Note: expresso doesn't support https as of now, see 
+// https://github.com/visionmedia/expresso/issues/129
+// need to fudge in the code in the meantime
+
 process.env.NODE_ENV = 'test';
-var app = require('../app');
+var app = require('../app'),  assert = require('assert');
 module.exports = {
-'GET /people.json': function(assert) {
+'GET /people.json': function() {
     assert.response(app,
       { url: '/people.json' },
-      { status: 200, headers: { 'Content-Type': 'application/json' }},
+      { status: 200, headers: { 'Content-Type': 'application/json; charset=utf-8' }},
       function(res) {
         var documents = JSON.parse(res.body);
         assert.type(documents, 'object');
@@ -16,12 +20,12 @@ module.exports = {
         });*/
       });
   },
-'GET /': function(assert) {
+'GET /': function() {
     assert.response(app,
       { url: '/' },
       { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
       function(res) {
-        assert.includes(res.body, '<title>W3C TPAC Web App</title>');
+        assert.includes(res.body, '<title>W3Connect</title>');
         process.exit();
       });
  }
