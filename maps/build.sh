e144ee939ad72e7b25c2467186ae067aac4af13d
Dominique Hazaël-Massieux <dom@w3.org>
2011-09-12 15:23:20 +0200

    parametrizing

--- maps/build.sh
+++ maps/build.sh
@@ -1,13 +1,15 @@
 # depends on inkscape to generate PNG versions
 
-if [ ! -d ../node.js/public/maps ] ; then mkdir ../node.js/public/maps ; fi
+DESTDIR = "$2"
+
+if [ ! -d "$DESTDIR" ] ; then mkdir "$DESTDIR" ; fi
 
 ROOMS_IDS=`cat rooms.json|cut -d ":" -f 2|cut -d "," -f 1|cut -d '"' -f 2|grep -v "\["|grep -v "\]"`
 ROOMS_SVG=`for i in $ROOMS_IDS; do echo "$i.svg" ; done`
 
 for i in $ROOMS_SVG
-do cat Santa_Clara_Marriott_Map.svg |sed -e 's/"text\/css">/"text\/css">#'`basename $i .svg`' { fill: yellow;}/' > $i; inkscape $i -e ../node.js/public/maps/`basename $i .svg`.png 2>/dev/null ; rm $i
+do cat "$1" |sed -e 's/"text\/css">/"text\/css">#'`basename $i .svg`' { fill: yellow;}/' > $i; inkscape $i -e "$DESTDIR"/`basename $i .svg`.png 2>/dev/null ; rm $i
 done
 
-cp Santa_Clara_Marriott_Map.svg ../node.js/public/maps/main.svg
-inkscape ../node.js/public/maps/main.svg -e ../node.js/public/maps/main.png
+cp "$1" "$DESTDIR"/main.svg
+inkscape "$DESTDIR"/main.svg -e "$DESTDIR"/maps/main.png
