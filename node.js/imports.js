fb3c3d74a35055c0c404a417ad1522aee1aecc16
Dominqique Hazael-Massieux <dom@w3.org>
2011-09-16 09:55:23 +0200

    bug fix: field from w3c json is id, not slug

--- node.js/imports.js
+++ node.js/imports.js
@@ -102,7 +102,7 @@ exports.importUserList = function(auth, callback)  {
 	    people.family = peopleData.family;
 	    people.email = peopleData.email;
 	    people.login = peopleData.login;
-	    people.slug = peopleData.slug;
+	    people.slug = peopleData.id;
 	    loadPeopleData(people.slug);
 	    if (peopleData.organization && peopleData.organization.slug) {
 		     people.affiliationId = peopleData.organization.slug;
