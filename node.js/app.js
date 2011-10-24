268f46dca83760791ef0e2a528e69ff37aa13888
Dominique Hazaël-Massieux <dom@w3.org>
2011-10-24 15:40:11 +0200

    make streams more time-resistant

--- node.js/app.js
+++ node.js/app.js
@@ -538,6 +538,7 @@ app.get('/locations/stream', function(req, res) {
     res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Connection", "keep-alive");
     res.writeHead(200);
+    setInterval(function() { res.write(":\n"); }, 30);
     emitter.on("checkin", function(user, left, entered) {
 	res.write("data: " + JSON.stringify({"user": user, "left": left, "entered": entered, "you": (req.user && JSON.stringify(user._id) == JSON.stringify(req.user._id))}) + "\n\n");
     });
