a9bd01d215f2dc367ecf3649a697b1d7e918e550
Dominique Hazaël-Massieux <dom@w3.org>
2011-10-28 15:27:52 +0200

    timezone offset as integer

--- node.js/app.js
+++ node.js/app.js
@@ -844,7 +844,7 @@ app.all("/schedule/events/:slug/admin", function(req, res, next) {
 	if (err) {
 	    next();
 	}
-        res.render("schedule/event-admin", {locals: {title: "Update " + event.name, event: event, places: places, timezone_offset: config.schedule.timezone_offset}});
+        res.render("schedule/event-admin", {locals: {title: "Update " + event.name, event: event, places: places, timezone_offset: parseInt(config.schedule.timezone_offset, 10)}});
     });
   });
 });
