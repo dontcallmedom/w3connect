7e29e95396d84c78f5ee8e2a312d90e7ff37a79e
Dominique Hazaël-Massieux <dom@w3.org>
2012-07-19 16:03:36 +0200

    factor 1000 in time calculation

--- node.js/app.js
+++ node.js/app.js
@@ -274,7 +274,7 @@ function parseDate(datestring) {
 };
 
 function elapsedTime(time) {
-    var elapsedTime = Date.now() - time ;
+    var elapsedTime = (Date.now() - time) / 1000 ;
     var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
     var months = ["January", "February", "March", "April", "May",   
 		  "June", "July", "August", "September", "October", "November", "December"];
