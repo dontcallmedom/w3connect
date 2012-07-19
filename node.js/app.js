adad5d7a79b31c8053cf5b9d95c1e8759ae0953f
tgambet <none@none>
2012-07-19 15:00:45 +0200

    + links for room names on the homepage

--- node.js/app.js
+++ node.js/app.js
@@ -231,7 +231,7 @@ emitter.on("twitterListChange", function (id) {
 
 // Record events as statusupdates
 emitter.on("checkin", function(user, left, entered) {
-    var status  = new Status({author: user, time: Date.now(), statusType: "checkin", content: /*user.given + " " + user.family + */(left && left.name ? " left " + left.name + (entered && entered.name ? " and " : "") : "") + (entered && entered.name ? " entered " + entered.name : "")/* + "."*/});
+    var status  = new Status({author: user, time: Date.now(), statusType: "checkin", content: (left && left.name ? " left <a href='" + baseurl + "/locations/" + left.shortname + "'>" + left.name + "</a>" + (entered && entered.name ? " and " : "") : "") + (entered && entered.name ? " entered <a href='" + baseurl + "/locations/" + entered.shortname + "'>" + entered.name + "</a>" : "")/* + "."*/});
     status.save();
 });
 
@@ -239,7 +239,7 @@ emitter.on("newevent", function(event) {
     var time = event.time;
     if (time) {
 	time.setUTCHours(time.getUTCHours() + parseInt(config.schedule.timezone_offset, 10));
-	var status  = new Status({author: event.proposedBy, time: Date.now(), statusType: "event", content: /*user.given + " " + user.family + */" scheduled a new ad-hoc event: " + event.name + " at " + time + " in " + event.room.name/* + "."*/});
+	var status  = new Status({author: event.proposedBy, time: Date.now(), statusType: "event", content: " scheduled a new ad-hoc event: " + event.name + " at " + time + " in <a href='" + baseurl + "/locations/" + event.room.shortname + "'>" + event.room.name + "</a>"});
 	status.save();
     }
 });
