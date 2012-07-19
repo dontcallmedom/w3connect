6f55a68288eca69b93f106255eca30e81f33920f
Dominique Hazaël-Massieux <dom@w3.org>
2012-07-19 16:27:58 +0200

    fix filtering of people

--- node.js/app.js
+++ node.js/app.js
@@ -861,25 +861,27 @@ app.get('/people/:letter?.:format?', function (req, res, next){
   if  (!(letter=='all' || letter.match(/^[a-z]$/))) {
       next();
   }
-  var filter = new RegExp('^' + letter, 'i');
-  if (letter=='all') {
-      filter = new RegExp('.*');
-  }
   var people = People.find({})
-	        .where('family', filter)
 		.run(function (err, people) {
     var activeLetters = {};
-    people.sort(function (a,b) { return (a.family > b.family ? 1 : (b.family > a.family ? -1 : 0));});
+    var letterPeople = [];
+    people.sort(function (a,b) { return (a.family > b.family ? 1 : (b.family > a.family ? -1 : 0));});	
     for (var p in people) {
 	activeLetters[people[p].family[0]]++;
+	if (letter != 'all' && people[p].family[0].toLowerCase() == letter.toLowerCase()) {
+	    letterPeople.push(people[p]);
+	}
+    }
+    if (letter == 'all') {
+	letterPeople = people;
     }
     switch (req.params.format) {
       // When json, generate suitable data
       case 'json':
-        res.send(people);
+        res.send(letterPeople);
 	break;
       default:
-        res.render('people/index.ejs', { locals: { activeLetters: activeLetters, letter: letter, people: people, title: 'People — ' + letter}});
+        res.render('people/index.ejs', { locals: { activeLetters: activeLetters, letter: letter, people: letterPeople, title: 'People — ' + letter}});
     }
   });
   
