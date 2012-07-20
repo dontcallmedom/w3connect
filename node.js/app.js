d51144d2dca60518061a3abafa5d5456f9b567f4
Dominique Hazael-Massieux <dom@w3.org>
2012-07-20 17:27:36 +0200

    list letters of participants in uppercase fixes #5

--- node.js/app.js
+++ node.js/app.js
@@ -850,7 +850,7 @@ app.get('/people/:letter?.:format?', function (req, res, next){
     var letterPeople = [];
     people.sort(function (a,b) { return (a.family > b.family ? 1 : (b.family > a.family ? -1 : 0));});	
     for (var p in people) {
-	activeLetters[people[p].family[0]]++;
+	activeLetters[people[p].family[0].toUpperCase()]++;
 	if (letter != 'all' && people[p].family[0].toLowerCase() == letter.toLowerCase()) {
 	    letterPeople.push(people[p]);
 	}
