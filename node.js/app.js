e238757ba29fd46605a0f35989772576e01c5f93
Dominique Hazael-Massieux <dom@w3.org>
2012-10-28 22:15:03 +0100

    trying to smudge around lack of post data

--- node.js/app.js
+++ node.js/app.js
@@ -825,6 +825,7 @@ app.all('/locations/:id.:format?', function(req, res) {
 	    };
 	    // auto-check-in if nfc is set in the query string
 	    if (req.loggedIn && req.query["nfc"] === '') {
+		req.body.checking = true;
 		userCheckin(req, res, render, place)		
 	    } else {
 		render();
