e610e63aea332763757b128539cfba253ff1d66a
Dominique Hazael-Massieux <dom@w3.org>
2012-10-30 16:26:42 +0100

    typo

--- node.js/app.js
+++ node.js/app.js
@@ -1307,7 +1307,7 @@ app.all('/schedule/events/:slug.:format?', function(req, res, next) {
 		    res.send(event);
 		    break;
 		default:
-		    res.render("schedule/event.ejs", {event: event, title: event.name, proposedBy: event.proposedBy, people: interested});
+		    res.render("schedule/event.ejs", {event: event, title: event.name, proposedBy: event.proposedBy, people: event.interested});
 		}
 	    });
 });
